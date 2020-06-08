2373c37b5e8a43a37e98406a3818374f91b9e350
Yogesh Suthar <ysuthar.web@gmail.com>
2020-06-08 16:06:13 +0530

    Location updates

--- js/app.js
+++ js/app.js
@@ -258,3 +258,65 @@ document.querySelector('#regenerate-code').onclick = async () => {
   makeCode();
 };
 
+/** Geo Location **/ 
+if ("geolocation" in navigator) {
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setCurrentPosition, positionError, {
+        enableHighAccuracy: false,
+        timeout: 15000,
+        maximumAge: 0
+    });
+}
+
+function setCurrentPosition(position) {
+    document.querySelector('.accuracy').innerHTML = position.coords.accuracy;
+    //document.querySelector('.altitude').innerHTML = position.coords.altitude;
+    //document.querySelector('.altitudeAccuracy').innerHTML = position.coords.altitudeAccuracy;
+    document.querySelector('.heading').innerHTML = position.coords.heading;
+    document.querySelector('.latitude').innerHTML = position.coords.latitude;
+    document.querySelector('.longitude').innerHTML = position.coords.longitude;
+    document.querySelector('.speed').innerHTML = position.coords.speed;
+}
+
+function positionError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            console.error("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            console.error("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            console.error("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            console.error("An unknown error occurred.");
+            break;
+    }
+}
+
+
+var geoWatch;
+
+function startWatch() {
+    if (!geoWatch) {
+        if ("geolocation" in navigator && "watchPosition" in navigator.geolocation) {
+            geoWatch = navigator.geolocation.watchPosition(setCurrentPosition, positionError, {
+                enableHighAccuracy: false,
+                timeout: 15000,
+                maximumAge: 0
+            });
+        }
+    }
+}
+
+setInterval(function(){navigator.geolocation.getCurrentPosition(setCurrentPosition, positionError, {
+        enableHighAccuracy: false,
+        timeout: 15000,
+        maximumAge: 0
+});console.log('geo updated')},10000);
